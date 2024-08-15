9cbe8b9edfbc7e6d2a7ebe17abef9fd38e4d4a8f
MathisGD <mathis@morpho.xyz>
2024-08-15 22:24:33 +0200

    test: fix compilation in certora setup

--- certora/helpers/IMorphoHarness.sol
+++ certora/helpers/IMorphoHarness.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.21;
+pragma solidity 0.8.26;
 
 import {Id, MarketParams, Market, IMorpho} from "../../lib/morpho-blue/src/interfaces/IMorpho.sol";
 
